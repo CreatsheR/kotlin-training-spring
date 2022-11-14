1aa049c8a686b294d36490944a6f67a35be967c5
A. Heimlich <arne.heimlich@oeffentliche.de>
2022-11-14 11:19:19 +0100

    Aufgabe 2 fertig

--- playground/src/main/kotlin/de/iteratec/playground/PlaygroundApplication.kt
+++ playground/src/main/kotlin/de/iteratec/playground/PlaygroundApplication.kt
@@ -1,10 +1,16 @@
 package de.iteratec.playground
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
-class PlaygroundApplication
+@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
+class PlaygroundApplication: CommandLineRunner {
+    override fun run(vararg args: String?) {
+        println("Hello Spring!")
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<PlaygroundApplication>(*args)
